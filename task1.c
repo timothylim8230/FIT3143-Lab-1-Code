31d4cbac19dac753f8b1c156228853979bbc173e
scottnguy3n101 <scottnbusiness101@gmail.com>
2026-08-10 11:34:19 +1000

    implemented end loop logic that returns all primes below n

--- task1.c
+++ task1.c
@@ -1,11 +1,50 @@
 #include <stdio.h>;
+#include <math.h>;
 
 
 
 void primes_before_n(int n) {
 
+    // get sqrt of n 
+    int sqrtN = sqrt(n);
+
+    // initialise array to store if a number is prime or not
+    bool isPrime[n+1];
+
+    // set prime array to all true 
+     for (int i = 0; i <= n; i++) {
+        isPrime[i] = true;
+     }
+
+    // loop through all values below i until sqrt(n) to cross off non-primes
     for (int i = 0; i < n; i++) {
         
+        if (i >= sqrtN) {
+
+            // initialise prime counts 
+            int primeCount = 0;
+            
+            // increment primeCount for each true in prime array
+            for (int i = 0; i < n; i++) {
+                if (isPrime[i]) {
+                    primeCount++;
+                }
+            }
+
+            int primes[primeCount];
+            int index = 0;
+            for (int i = 1; i <= n; i++) {
+                if (isPrime[i]) {
+                    primes[index++] = i;
+                }
+            }
+
+            return primes;
+            
+            
+        }
+
+
     }
 
 }
